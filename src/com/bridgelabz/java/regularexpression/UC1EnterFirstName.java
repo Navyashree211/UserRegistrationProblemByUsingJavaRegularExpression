f1cc7a1e12c88d0c4b6bffc9db5b528448ea1f02
navyas211@gmail.com <navyas211@gmail.com>
2021-10-09 23:03:51 +0530

    [NAVYA]ADDED: As a user need to enter a valid First Name

---
+++ src/com/bridgelabz/java/regularexpression/UC1EnterFirstName.java
@@ -0,0 +1,20 @@
+/**
+ * Valid First Name with first letter starting with capital and has minimum 3 characters in it.
+ *
+ * @author: Navya Shree
+ * @since: 9.10.2021
+ */
+package com.bridgelabz.java.regularexpression;
+
+public class UC1EnterFirstName {   
+	public static void main(String[] args) {
+		System.out.println("Navya");
+		System.out.println("Anokhi");
+	}
+	//validate first name
+	public static boolean firstName(String FirstName )  {
+		return FirstName.matches("^[A-Z]{1}[a-z]{2,}$");
+		
+	}
+
+}
